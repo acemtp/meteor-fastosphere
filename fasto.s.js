c382f5bda072192550d7e52bb885922ad07c4cfe
Vianney Lecroart <acemtp@gmail.com>
2014-12-25 16:32:03 +0100

    send marked json

--- fasto.s.js
+++ fasto.s.js
@@ -1,16 +1,52 @@
 
-var changelog = function (packageName, currentVersion) {
+changelog = function (packageName, currentVersion) {
   var p = Packages.findOne({ 'atmo.name': packageName });
-  if (!p) return 'Package "'+packageName+'"" not found';
-  if (!p.changelog) return 'Package "'+packageName+'"" doesn\'t have changelog';
+  if (!p) { console.log('Package not found', packageName); return undefined; }
+  if (!p.changelog) { console.log('Package without changelog', packageName); return undefined; }
 
   var tokens = marked.lexer(p.changelog, { gfm: true });
+//  console.log('****************************');
+//  console.log(tokens);
+//  console.log('****************************');
 
+  for(var i = 0; i < tokens.length; i++) {
+    var t = tokens[i];
+    if(t.type === 'heading' && t.text.indexOf(currentVersion) !== -1) {
+//      console.log('found', t);
+      break;
+    }
+  }
 
+  if(i === tokens.length) { console.log('Version not found in changelog', packageName); return undefined; }
+
+  var subtokens = tokens.slice(0, i);
+
+//  subtokens.links = tokens.links;
+//  console.log('__-****************************');
+//  console.log(subtokens);
+//  console.log('****************************');
+/*
+  var str = '';
+  subtokens.forEach(function(t) {
+    if(t.type === 'heading')
+      str += '\n\n' + Array(t.depth).join('#') + ' ' + t.text + '\n\n';
+    else if(t.type === 'list_item_start')
+      str += '- ';
+    else if(t.type === 'text')
+      str += t.text + '\n';
+  });
+*/
+  return subtokens;
+//  return str;
+//  return marked.parser(subtokens);
 };
+
+//debug
+//console.log('cl', changelog("iron:router", '0.5.4'));
 
 
 var getPackages = function () {
+  console.log('getPackages');
   var cnx = DDP.connect('https://atmospherejs.com');
   var p = new Mongo.Collection('packages', { connection: cnx });
   var array = [];
@@ -41,57 +77,63 @@ var getPackages = function () {
   });
 };
 
-var updatePackages = function() {
-    Packages.find().forEach(function (p) {
-//  Packages.find({ 'atmo.name': 'iron:router' }, { limit: 10 }).forEach(function (p) {
-    var git = p && p.atmo && p.atmo.latestVersion && p.atmo.latestVersion.git || '';
+var updatePackage = function (p) {
+  var giturl = p && p.atmo && p.atmo.latestVersion && p.atmo.latestVersion.git || '';
 //    console.log('git', git);
-    if(!git) return;
+  if(!giturl) return;
 
-    var re = /^https?:\/\/github\.com\/([^\/]+)\/(.*?)(?:\.git)?$/gm; 
-    var m = re.exec(git);
+  var re = /^https?:\/\/github\.com\/([^\/]+)\/(.*?)(?:\.git)?$/gm; 
+  var m = re.exec(giturl);
 //    console.log('m', m);
 
-    if(!m) return;
+  if(!m) return;
 
-    var userAgent = "Meteor";
-    if (Meteor.release) userAgent += "/" + Meteor.release;
+  var userAgent = "Meteor";
+  if (Meteor.release) userAgent += "/" + Meteor.release;
 
-    // get info on repo
-    HTTP.get('https://api.github.com/repos/'+m[1]+'/'+m[2]+'?client_id=000dbbc3c779d4426d8e&client_secret=b57e2f9830282014280e426df412d5921a825be6', { headers: { Accept: 'application/json', "User-Agent": userAgent } }, function (err, res) {
-      if(err) return console.error('a', m, err);
+  // get info on repo
+  HTTP.get('https://api.github.com/repos/'+m[1]+'/'+m[2]+'?client_id=000dbbc3c779d4426d8e&client_secret=b57e2f9830282014280e426df412d5921a825be6', { headers: { Accept: 'application/json', "User-Agent": userAgent } }, function (err, res) {
+    if(err) return console.error('a', m, err);
 //      console.log('res', res);
-      Packages.update(p._id, { $set: { git: res.data } });
-    });
+    Packages.update(p._id, { $set: { git: res.data } });
+  });
 
-    // list files in root directory
-    HTTP.get('https://api.github.com/repos/'+m[1]+'/'+m[2]+'/contents'+'?client_id=000dbbc3c779d4426d8e&client_secret=b57e2f9830282014280e426df412d5921a825be6', { headers: { Accept: 'application/json', "User-Agent": userAgent } }, function (err, res) {
-      if(err) return console.error('b', m, err);
+  // list files in root directory
+  HTTP.get('https://api.github.com/repos/'+m[1]+'/'+m[2]+'/contents'+'?client_id=000dbbc3c779d4426d8e&client_secret=b57e2f9830282014280e426df412d5921a825be6', { headers: { Accept: 'application/json', "User-Agent": userAgent } }, function (err, res) {
+    if(err) return console.error('b', m, err);
 
-      res.data.forEach(function(f) {
-        if(_.indexOf(['history.md', 'changelog.md'], f.name.toLowerCase()) != -1) {
-          console.log('found', p.atmo.name, f.name);
+    res.data.forEach(function(f) {
+      if(_.indexOf(['history.md', 'changelog.md'], f.name.toLowerCase()) != -1) {
+        console.log('found', p.atmo.name, f.name);
 
-          HTTP.get(f.download_url, function (err, res) {
-            if(err) return console.error(err);
+        HTTP.get(f.download_url, function (err, res) {
+          if(err) return console.error(err);
 //            console.log('res', res);
-            Packages.update(p._id, { $set: { changelog: res.content } });
-          });
-        }
-      });
+          Packages.update(p._id, { $set: { changelog: res.content } });
+        });
+      }
+    });
 
 //      console.log('res', res);
 //      Packages.update(p._id, { $set: { git: res.data } });
-    });
+  });
+};
 
-
+var updatePackages = function() {
+  Packages.find().forEach(function (p) {
+//  Packages.find({ 'atmo.name': 'iron:router' }, { limit: 10 }).forEach(function (p) {
+    updatePackage(p);
   });
 };
 
 Meteor.startup(function () {
-//  getPackages();
+  getPackages();
 //  updatePackages();
 
+  var m = Packages.findOne({ name: 'METEOR' });
+  if(!m) Packages.insert({ name: 'METEOR', atmo: { name: 'METEOR', latestVersion: { git: 'https://github.com/meteor/meteor' } } });
+
+  updatePackage(Packages.findOne({ name: 'METEOR' }));
 
 //  console.log('res', JSON.stringify(packages));
 
--- fasto_api.s.js
+++ fasto_api.s.js
@@ -1,8 +1,35 @@
 Router.route('/api/changelog', { where: 'server' })
   .post(function () {
     // POST /webhooks/stripe
-    console.log(this.request.body.packages)
-    console.log(this.request.body.versions)
+//    console.log(this.request.body.release);
+    console.log(this.request.body.versions);
 
-    this.response.end('POST Here is the changelog\n');
+    var str = '';
+
+    var packages = this.request.body.versions.split('\n');
+
+//console.log('pac', packages);
+
+    var tokens = [];
+
+  	packages.forEach(function (p) {
+      var pa = p.split('@');
+      if(pa.length !== 2) return;
+      var subtokens = changelog(pa[0], pa[1]);
+      if(subtokens) {
+        var title = pa[0] + ' (local ' +  pa[1] + ')';
+        tokens.push({ type: 'heading', depth: 1, text: title });
+        tokens = tokens.concat(subtokens);
+/*
+        var title = pa[0] + ' (local ' +  pa[1] + ')';
+        str += '\n\n# ' + title + '\n\n';
+        str += cl;
+*/
+      }
+  	});
+
+    console.log('tok', tokens);
+
+//console.log('str', str);
+    this.response.end(JSON.stringify(tokens));
   });
