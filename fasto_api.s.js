83e2192bf9864f6b45c75bf8bbd1dd00377fa16d
peernohell <peernohell@gmail.com>
2014-12-25 15:10:00 +0100

    npm nodule request meteor api to get information about vresions and packages

--- fasto_api.s.js
+++ fasto_api.s.js
@@ -1,7 +1,8 @@
 Router.route('/api/changelog', { where: 'server' })
   .post(function () {
     // POST /webhooks/stripe
-    console.log('POST THIS IS NEW', this);
-    console.log('P', this.params);
+    console.log(this.request.body.packages)
+    console.log(this.request.body.versions)
+
     this.response.end('POST Here is the changelog\n');
   });
